35a78861d8e7df232d5ca5d22de2ce75702a1d0c
Peter Dedene <peter@zenjoy.be>
2023-04-27 11:13:07 +0200

    Properly support multiline outputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 
 OUTPUT=$(sh -c "curl --silent --show-error --fail $*")
 
-echo "curl_output=\"$OUTPUT\"" | tee -a $GITHUB_OUTPUT
+echo -e "curl_output<<EOF\n$OUTPUT\nEOF" | tee -a $GITHUB_OUTPUT
