215785ad1728ce95241be51b756a3661202a0b79
Peter Dedene <peter@zenjoy.be>
2023-04-27 11:28:55 +0200

    We need newlines here

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ set -e
 OUTPUT=$(sh -c "curl --silent --show-error --fail $*")
 
 delimiter="$(openssl rand -hex 8)"
-echo -e "curl_output<<${delimiter}${OUTPUT}${delimiter}" | tee -a $GITHUB_OUTPUT
+echo -e "curl_output<<${delimiter}\n${OUTPUT}\n${delimiter}" | tee -a $GITHUB_OUTPUT
